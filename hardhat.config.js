f4799aa9a505261a6f650af1c008e23e11a2cf3e
Akay <akaudeh@gmail.com>
2021-12-04 22:54:48 +0100

    removed private key

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     rinkeby: {
       url: 'https://eth-rinkeby.alchemyapi.io/v2/mVGk9rWiY_2911RQmJx68wRQgvBvKA08',
-      accounts: ['e185522471419b1e12b6194f5c8c669f482f9301f8fdbc4358a4cb7942e52332'],
+      accounts: [''],
     },
   },
 };
